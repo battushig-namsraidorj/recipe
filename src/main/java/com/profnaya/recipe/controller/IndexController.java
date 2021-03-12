c8dc2d9d8eb54ad11eebb9c547fa10344a9faca5
Battushig Namsraidorj <battushig-namsraidorj@gmail.com>
2021-03-11 18:05:46 -0600

    Added findByDescription(desc) JPA Query method in CategoryRepository and UnitOfMeasureRepository. Closes #8

--- src/main/java/com/profnaya/recipe/controller/IndexController.java
+++ src/main/java/com/profnaya/recipe/controller/IndexController.java
@@ -1,14 +1,30 @@
 package com.profnaya.recipe.controller;
 
+import com.profnaya.recipe.domain.Category;
+import com.profnaya.recipe.domain.UnitOfMeasure;
+import com.profnaya.recipe.repository.CategoryRepository;
+import com.profnaya.recipe.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"","/","index"})
     public String getIndexPage() {
-        System.out.println("Hello World! sds");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptinal = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptinal.get().getId());
         return "index";
     }
 }
--- src/main/java/com/profnaya/recipe/repository/CategoryRepository.java
+++ src/main/java/com/profnaya/recipe/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.profnaya.recipe.repository;
 import com.profnaya.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
