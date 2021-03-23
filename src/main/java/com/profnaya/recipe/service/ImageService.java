6500a93fe44d3a8f39dc0c4ee0757e85af75ad8e
Battushig Namsraidorj <battushig-namsraidorj@gmail.com>
2021-03-22 19:23:37 -0500

    upload form, controller and service mocks done. Closes #29

---
+++ src/main/java/com/profnaya/recipe/service/ImageService.java
@@ -0,0 +1,7 @@
+package com.profnaya.recipe.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
